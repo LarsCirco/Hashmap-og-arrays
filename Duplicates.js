b8b2bc8d2f763279cd4bf0a9ea9d5bd91f3b671b
LarsCirco <122366416+LarsCirco@users.noreply.github.com>
2023-08-07 11:27:15 +0200

    ferdig

--- Duplicates.js
+++ Duplicates.js
@@ -1,1 +1,18 @@
-dsf
+/* Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.*/
+
+let nums = [1,1,1,3,3,4,3,2,4,2]
+
+var containsDuplicate = function(nums)  {
+    for (let i = 0; i<nums.length; i++)
+    {
+        for (let j = 1; j<nums.length; j++){
+        
+           let duplicate = nums[j] == nums[i]
+
+            if(duplicate)
+            return true;
+        }
+        return false;
+    }
+};
+https://leetcode.com/problems/contains-duplicate/submissions/1014545565/
